39b6f5418a3092779688ed333ba85f989ef85f33
Allan Christien <Christien.allanpro@gmail.com>
2021-10-14 15:44:38 +0200

    Ajout des classes médicaments

---
+++ src/gsb/service/MedicamentService.java
@@ -0,0 +1,21 @@
+package gsb.service;
+
+import gsb.modele.Medicament;
+import gsb.modele.dao.MedicamentDao;
+
+public class MedicamentService {
+
+	public static Medicament rechercherMedicament(String medDepotLegal) {
+		Medicament unMedicament = null;
+		try {
+			if (medDepotLegal == null) {
+				throw new Exception("Donnée obligatoire : DepotLegal");
+			}
+			unMedicament = MedicamentDao.rechercher(medDepotLegal);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return unMedicament;
+	}
+
+}
---
+++ src/gsb/tests/MedicamentDaoTest.java
@@ -0,0 +1,10 @@
+package gsb.tests;
+import gsb.modele.Medicament;
+import gsb.modele.dao.MedicamentDao;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class MedicamentDaoTest {
+
+}
